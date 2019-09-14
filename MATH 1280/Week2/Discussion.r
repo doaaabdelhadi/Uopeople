b2305574e0984d85d3bf76f3e9c26836675cd694
Doaa Mohaemd <duaamuha10@yahoo.com>
2019-09-14 03:28:24 +0200

    Update Discussion.r

--- MATH 1280/Week2/Discussion.r
+++ MATH 1280/Week2/Discussion.r
@@ -1,3 +1,4 @@
+// part 1
 f <-c('Apple','Apple','Banana','Orange')
 data.frame(table(f))
 // run
@@ -7,9 +8,13 @@ f Freq
 2 Banana    1
 3 Orange    1
 */
+
+// part 2
 Grades <-c(94,93,85,0)
 mean(Grades)
 // mean of Grades = 68
+
+// Part 3
 Grade_s <-('A','A','B','F')
 data.frame(table(Grade_s))
 /*
