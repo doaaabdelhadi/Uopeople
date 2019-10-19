d7031be96d4fdb1a59ae2d51a6b1f406ba7d31a9
Doaa Mohaemd <duaamuha10@yahoo.com>
2019-10-19 19:33:17 +0200

    Create main.java

---
+++ CS 1102/box/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package box;
+
+/**
+ *
+ * @author Dode22
+ */
+public class main {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        MatchBox b1 = new MatchBox(12,15,2,16); 
+        b1.getVolume();
+        b1.calculateWeight();
+   
+        //Box box1 = new Box();
+    }
+}
